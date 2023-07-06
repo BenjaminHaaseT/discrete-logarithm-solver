984da5975453509b9247d8f73484bbdd5c1661b8
Benjamin Haase <benjaminthaase@protonmail.com>
2023-07-06 13:02:45 -0700

    Added the main collision algorithm, has not been stress tested at all, probably needs to be debugged

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::error::Error;
 
 pub fn is_prime(p: u32) -> bool {
@@ -51,6 +52,40 @@ impl FpUnitsDiscLogSolver {
             n += 1;
         }
         n
+    }
+
+    pub fn disc_logarithm(&self, base: u32, radix: u32) -> Option<u32> {
+        assert!(base % self.prime != 0);
+        // First compute order of base
+        let order = self.compute_order(base);
+        let n = f32::floor(f32::sqrt(order as f32)) as u32 + 1;
+
+        // Generate our lists
+        let mut list1 = HashMap::new();
+        list1.insert(1, 0);
+        let mut g = base;
+
+        for i in 1..=(n as i32) {
+            list1.insert(g, i);
+            g *= base;
+            g %= self.prime;
+        }
+
+        let mut u = 1;
+        let base_inverse = self.compute_inverse(g);
+
+        for i in 0..=(n as i32) {
+            // First check if we have a match
+            if let Some(x) = list1.get(&((radix * u) % self.prime)) {
+                let res = x + (i * (n as i32));
+                return Some(res as u32);
+            }
+            // Otherwise update value of u
+            u *= base_inverse;
+            u %= self.prime;
+        }
+
+        None
     }
 }
 
