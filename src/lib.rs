98d1a8fd078ef212c3218c204b0bc98084bf7d2f
Benjamin Haase <benjaminthaase@protonmail.com>
2023-07-07 13:31:02 -0700

    fixed bug in shanks algorithm, all tests appear to be passing

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub fn is_prime(p: u32) -> bool {
 }
 
 pub struct FpUnitsDiscLogSolver {
-    prime: u32,
+    pub prime: u32,
 }
 
 impl FpUnitsDiscLogSolver {
@@ -54,29 +54,31 @@ impl FpUnitsDiscLogSolver {
         n
     }
 
-    pub fn disc_logarithm(&self, base: u32, radix: u32) -> Option<u32> {
-        assert!(base % self.prime != 0);
+    pub fn shanks_algorithm(&self, base: u32, num: u32) -> Option<u32> {
+        assert!(base % self.prime != 0 && num % self.prime != 0);
         // First compute order of base
         let order = self.compute_order(base);
         let n = f32::floor(f32::sqrt(order as f32)) as u32 + 1;
+        println!("{}", n);
 
         // Generate our lists
         let mut list1 = HashMap::new();
-        list1.insert(1, 0);
-        let mut g = base;
+        let mut g = 1;
 
-        for i in 1..=(n as i32) {
+        for i in 0..(n as i32) {
             list1.insert(g, i);
             g *= base;
             g %= self.prime;
         }
+        // Insert final value for base and exponent into first list
+        list1.insert(g, n as i32);
 
         let mut u = 1;
         let base_inverse = self.compute_inverse(g);
 
         for i in 0..=(n as i32) {
-            // First check if we have a match
-            if let Some(x) = list1.get(&((radix * u) % self.prime)) {
+            // Check if we have a match
+            if let Some(x) = list1.get(&((num * u) % self.prime)) {
                 let res = x + (i * (n as i32));
                 return Some(res as u32);
             }
@@ -84,9 +86,10 @@ impl FpUnitsDiscLogSolver {
             u *= base_inverse;
             u %= self.prime;
         }
-
         None
     }
+
+    // pub fn shanks_algorithm_with_lists
 }
 
 #[cfg(test)]
@@ -103,6 +106,15 @@ mod tests {
         println!("g * g^-1 = {}", (base * inverse) % 71);
 
         assert_eq!((base * inverse) % 71, 1);
+
+        let solver = FpUnitsDiscLogSolver::new(17389);
+        let base = 9704;
+        let inverse = solver.compute_inverse(9704);
+
+        println!("g = {}, g inverse = {}", base, inverse);
+        println!("g * g^-1 = {}", (base * inverse) % 17389);
+
+        assert_eq!((base * inverse) % 17389, 1);
     }
 
     #[test]
@@ -114,5 +126,34 @@ mod tests {
         println!("g^n = {}", solver.fast_power(base, order));
 
         assert_eq!(solver.fast_power(base, order), 1);
+
+        let solver = FpUnitsDiscLogSolver::new(17389);
+        let base = 9704;
+        let order: u32 = solver.compute_order(base);
+        println!("g = {}, order of g = {}", base, order);
+        println!("g^n = {}", solver.fast_power(base, order));
+
+        assert_eq!(solver.fast_power(base, order), 1);
+    }
+
+    #[test]
+    fn test_shanks_algorithm() {
+        let solver = FpUnitsDiscLogSolver::new(71);
+        let base = 11;
+        if let Some(x) = solver.shanks_algorithm(base, 23) {
+            println!("{}^{} = {}", base, x, solver.fast_power(base, x));
+            assert_eq!(solver.fast_power(base, x), 23);
+        } else {
+            panic!();
+        }
+
+        let solver = FpUnitsDiscLogSolver::new(17389);
+        let base = 9704;
+        if let Some(x) = solver.shanks_algorithm(base, 13896) {
+            println!("{}^{} = {}", base, x, solver.fast_power(base, x));
+            assert_eq!(solver.fast_power(base, x), 13896);
+        }
+
+        assert!(true);
     }
 }
