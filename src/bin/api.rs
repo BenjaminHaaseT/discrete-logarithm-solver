44851a59c0e2f3118654e6d3704c907e60689c81
Benjamin Haase <benjaminthaase@protonmail.com>
2023-07-14 11:57:33 -0700

    Got error handling correct, the solve endpoint now works as expected, returns all the information useful for a frontend application

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,6 +1,8 @@
 use actix_web::body::BoxBody;
 use actix_web::{
-    get, http::header::ContentType, post, web, App, HttpResponse, HttpServer, Responder,
+    error, get,
+    http::{header::ContentType, StatusCode},
+    post, web, App, HttpResponse, HttpServer, Responder,
 };
 use discrete_logarithm_lib::{is_prime, shanks_algorithm_with_output, FpUnitsDiscLogSolver};
 use serde::{Deserialize, Serialize};
@@ -36,6 +38,37 @@ impl Responder for ShanksOutput {
             .body(body)
     }
 }
+
+#[derive(Debug)]
+enum UserError {
+    BadInput(String),
+    InternalServer,
+}
+
+impl std::fmt::Display for UserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            UserError::BadInput(ref s) => write!(f, "{s}"),
+            UserError::InternalServer => {
+                write!(f, "An internal error occured. Please try again later.")
+            }
+        }
+    }
+}
+
+impl error::ResponseError for UserError {
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        HttpResponse::build(self.status_code()).body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            UserError::BadInput(_) => StatusCode::BAD_REQUEST,
+            UserError::InternalServer => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
 // struct AppSolver {
 //     solver: Mutex<Option<FpUnitsDiscLogSolver>>,
 // }
@@ -75,17 +108,32 @@ impl Responder for ShanksOutput {
 // }
 
 #[get("/solve")]
-async fn solve_discrete_logarithm(data: web::Json<Inputs>) -> impl Responder {
+async fn solve_discrete_logarithm(data: web::Json<Inputs>) -> Result<ShanksOutput, UserError> {
+    // Get input from json
     let (prime, base, num) = (data.prime, data.base, data.num);
-    if !is_prime(prime) || base % prime == 0 || num % prime == 0 {
-        return HttpResponse::Conflict().body("invalid input");
+    // Check input is valid
+    if !is_prime(prime) {
+        return Err(UserError::BadInput(format!("{} is not prime", prime)));
+    } else if base % prime == 0 {
+        return Err(UserError::BadInput(format!(
+            "{} is not a valid base, {} mod {} = 0",
+            base, base, prime
+        )));
+    } else if num % prime == 0 {
+        return Err(UserError::BadInput(format!(
+            "{} has no logarithm mod {}, since {} mod {} = 0",
+            num, prime, num, prime
+        )));
     }
 
-    let (log, base_inverse, n, list1, list2) = shanks_algorithm_with_output(prime, base, num);
-    let log = if let Some(x) = log { x } else { 0 };
+    // Get return data from solving discrete logarithm, if the log = 0 after matching we know no discrete logarithm exists
+    let (log, base_inverse, n, list1, list2) = match shanks_algorithm_with_output(prime, base, num)
+    {
+        (Some(l), inv, ord, l1, l2) => (l, inv, ord, l1, l2),
+        (None, inv, ord, l1, l2) => (0, inv, ord, l1, l2),
+    };
 
-    // TODO: Figure out how to return error and output from same function.
-    ShanksOutput {
+    Ok(ShanksOutput {
         prime,
         base,
         num,
@@ -94,8 +142,16 @@ async fn solve_discrete_logarithm(data: web::Json<Inputs>) -> impl Responder {
         n,
         collision_list1: list1,
         collision_list2: list2,
-    }
+    })
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {}
+async fn main() -> std::io::Result<()> {
+    let address = "127.0.0.1";
+    let port = 8080;
+
+    HttpServer::new(move || App::new().service(solve_discrete_logarithm))
+        .bind((address, port))?
+        .run()
+        .await
+}
